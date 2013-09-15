76c263d58b46e37cfa1a7ddf544a40ff48b1a6c9
Chelsey Frank <chelsey@zaarly.com>
2013-09-14 19:06:50 -0700

    Beefed out events controller to include a show for ind events

--- events/app/controllers/events_controller.rb
+++ events/app/controllers/events_controller.rb
@@ -1,7 +1,10 @@
 class EventsController < ApplicationController
 
 	def index
-		@events = ['BugSmash', 'Hackathon', 'Kata Camp', 'Rails User Group']
+		@events = Event.all
 	end
 
+	def show
+		@event = Event.find(params[:id])
+	end
 end
