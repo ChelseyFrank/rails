d8bd46cc7ba9d01100432cfbd966bbfa94601d91
Chelsey Frank <chelsey@zaarly.com>
2013-09-25 13:37:02 -0700

    Added edit route, action, view & link from the show page

--- events_test/config/routes.rb
+++ events_test/config/routes.rb
@@ -1,7 +1,6 @@
 EventsTest::Application.routes.draw do
   root "events#index"
-
-  get "events"			=>		"events#index"
-
-  get "events/:id"	=>		"events#show", as: "event"
+  get "events"					=>		"events#index"
+  get "events/:id"			=>		"events#show", as: "event"
+  get "events/:id/edit"	=>		"events#edit", as: "edit_event"
 end
