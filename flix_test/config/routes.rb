fe8d1fdc5bff79de1a78a5502e26082aaee54701
Chelsey Frank <chelsey@zaarly.com>
2013-09-25 17:01:08 -0700

    added edit link in show, edit action & spec

--- flix_test/config/routes.rb
+++ flix_test/config/routes.rb
@@ -1,7 +1,7 @@
 FlixTest::Application.routes.draw do
   root "movies#index"	
 
-  get "movies" 			=>  "movies#index"
-
-  get "movies/:id"	=>	"movies#show", as: "movie"
+  get "movies" 					=>  "movies#index"
+  get "movies/:id"			=>	"movies#show", as: "movie"
+  get "movies/:id/edi"	=>	"movies#edit", as: "edit_movie"
 end
