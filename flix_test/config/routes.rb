ecd6b10cf4896e99cff9ff3c23f744e0776102af
Chelsey Frank <chelsey@zaarly.com>
2013-09-25 17:02:02 -0700

    Fixed spelling error in routes

--- flix_test/config/routes.rb
+++ flix_test/config/routes.rb
@@ -3,5 +3,5 @@ FlixTest::Application.routes.draw do
 
   get "movies" 					=>  "movies#index"
   get "movies/:id"			=>	"movies#show", as: "movie"
-  get "movies/:id/edi"	=>	"movies#edit", as: "edit_movie"
+  get "movies/:id/edit"	=>	"movies#edit", as: "edit_movie"
 end
