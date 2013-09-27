8d051b4d93ccf2f61c83f41b60dae2cb49f9f8fe
Chelsey Frank <chelsey@zaarly.com>
2013-09-26 23:34:28 -0700

    Used resources for routes instead

--- events_test/config/routes.rb
+++ events_test/config/routes.rb
@@ -1,8 +1,5 @@
 EventsTest::Application.routes.draw do
   root "events#index"
-  get "events"					=>		"events#index"
-  get "events/:id"			=>		"events#show", as: "event"
-  get "events/:id/edit"	=>		"events#edit", as: "edit_event"
 
-  patch "events/:id"		=>		"events#update"
+  resources :events
 end
