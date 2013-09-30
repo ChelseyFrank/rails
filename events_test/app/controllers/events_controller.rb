90f7578e73c163aa4c93c263fbed196994aa253f
ChelseyFrank <chelsey@zaarly.com>
2013-09-29 20:45:28 -0700

    Added new schema to be editable

--- events_test/app/controllers/events_controller.rb
+++ events_test/app/controllers/events_controller.rb
@@ -41,6 +41,6 @@ private
 
 	def event_params
 		params.require(:event).
-										permit(:name, :description, :location, :price, :starts_at)
+										permit(:name, :description, :location, :price, :starts_at, :image_file_name, :capacity)
 	end
 end
