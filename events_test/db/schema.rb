2d1ae5f1a5aa903a6e55b69cd00a9912de3c53fc
ChelseyFrank <chelsey@zaarly.com>
2013-09-29 20:44:10 -0700

    db migration

--- events_test/db/schema.rb
+++ events_test/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130924225910) do
+ActiveRecord::Schema.define(version: 20130930025526) do
 
   create_table "events", force: true do |t|
     t.string   "name"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20130924225910) do
     t.datetime "starts_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.integer  "capacity",        default: 1
   end
 
 end
