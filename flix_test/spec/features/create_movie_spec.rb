34129d100a7c2f5fe924768b5cec67a1182e93c2
ChelseyFrank <chelsey@zaarly.com>
2013-09-29 21:02:45 -0700

    Beefed up specs

--- flix_test/spec/features/create_movie_spec.rb
+++ flix_test/spec/features/create_movie_spec.rb
@@ -13,6 +13,11 @@ describe "Creating a new movie" do
     fill_in "Rating", with: "PG-13"
     fill_in "Total gross", with: "75000000"
     select (Time.now.year - 1).to_s, :from => "movie_released_on_1i"
+    
+    fill_in "Cast", with: "Award-winning"
+    fill_in "Director", with: "Wes Anderson"
+    fill_in "Duration", with: "123 min"
+    fill_in "Image file name", with: "movie.png"
 
     # If you're taking advantage of the HTML 5 date field in Chrome, 
     # you'll need to use 'fill_in' rather than 'select'
